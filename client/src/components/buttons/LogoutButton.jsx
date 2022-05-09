db0e16ca5b0bd6b94ed9b3f7d9e547c71868b6b3
sptungG <ngoviettung154@gmail.com>
2022-05-09 22:45:22 +0700

    update logout

---
+++ client/src/components/buttons/LogoutButton.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "antd";
+import { FiLogOut } from "react-icons/fi";
+import { signOut } from "firebase/auth";
+import { auth } from "common/firebase-config";
+import { useUserStorage } from "common/useUserStorage";
+import { useNavigate } from "react-router-dom";
+
+const LogoutButton = ({ iconSize = 24, btnType = "text", children }) => {
+  const { setCredential } = useUserStorage();
+  let navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setCredential(null, null);
+      navigate("/");
+    } catch (error) {
+      console.log("handleLogout ~ error", error);
+    }
+  };
+
+  return (
+    <Button className="btn-logout" type={btnType} size="large" onClick={handleLogout}>
+      <div style={{ display: "flex", flexWrap: "nowrap", gap: 4, alignItems: "center" }}>
+        {iconSize > 0 && <FiLogOut size={iconSize} />}
+        <div>{children}</div>
+      </div>
+    </Button>
+  );
+};
+
+export default LogoutButton;
--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,10 +1,21 @@
+import { Space } from "antd";
+import { useUserStorage } from "common/useUserStorage";
+import LogoutButton from "components/buttons/LogoutButton";
 import MainLayout from "pages/MainLayout";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const { credential, setCredential } = useUserStorage();
   return (
     <MainLayout>
       HomePage
+      <div>user: {JSON.stringify(credential.user)}</div>
+      <Space size={8}>
+      <LogoutButton iconSize={18}>Đăng xuất</LogoutButton>
+      <Link to={"/login"}>Đăng nhập</Link>
+      <Link to={"/register"}>Đăng kí</Link>
+      </Space>
     </MainLayout>
   );
 };
