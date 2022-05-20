707df2a9e2890a25c86b6d59b7f3b4a1b04048b2
sptungG <ngoviettung154@gmail.com>
2022-05-20 17:08:14 +0700

    update category

---
+++ server/modules/category/category.controller.js
@@ -0,0 +1,61 @@
+const Category = require("./model.category");
+const Product = require("../product/model.product");
+
+exports.getAllCategory = async (req, res) => {
+  const { status } = req.query;
+  const categoryList = await Category.find({ status }).sort({ createdAt: -1 }).exec();
+  res.json(categoryList);
+};
+
+// exports.getFilteredCategory = async (req, res) => {};
+
+exports.getCategoryById = async (req, res) => {
+  const { id } = req.params;
+  const category = await Category.findOne({ _id: id, status: "active" }).exec();
+  const products = await Product.find({ category }).populate("category").exec();
+  res.json({
+    category,
+    products,
+  });
+};
+
+exports.createCategory = async (req, res) => {
+  try {
+    const { name, image } = req.body;
+    const category = await new Category({ name, image }).save();
+    res.json(category);
+  } catch (err) {
+    // console.log(err);
+    res.status(400).send("Create category failed");
+  }
+};
+
+exports.updateCategory = async (req, res) => {
+  const { id } = req.params;
+  const { name, image, status } = req.body;
+  try {
+    const updated = await Category.findOneAndUpdate(
+      { _id: id },
+      { name, image, status },
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(400).send("Category update failed");
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Category.findOneAndUpdate({ _id: id }, { status: "deleted" }, { new: true });
+
+    await Product.updateMany({
+      option: { category: id },
+      data: { status: "deleted" },
+    });
+    res.json("deleted successfully");
+  } catch (err) {
+    res.status(400).send("Category delete failed");
+  }
+};
--- server/modules/category/category.router.js
+++ server/modules/category/category.router.js
@@ -5,10 +5,18 @@ const router = express.Router();
 // middlewares
 const { authCheck, adminCheck } = require("../auth/auth.validation");
 // controllers
-
+const {
+  getAllCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategoryById,
+} = require("./category.controller");
 // routes
-router.get("/category", authCheck, (req, res) => {
-  res.json("category");
-});
+router.post("/category", authCheck, adminCheck, createCategory);
+router.get("/categories", getAllCategory);
+router.get("/category/:id", getCategoryById);
+router.put("/category/:id", authCheck, adminCheck, updateCategory);
+router.delete("/category/:id", authCheck, adminCheck, deleteCategory);
 
 module.exports = router;
