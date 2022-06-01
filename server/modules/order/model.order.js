72517929b66486ba3ab6bee1acb5da387171d434
sptungG <ngoviettung154@gmail.com>
2022-06-01 21:46:44 +0700

    update order payment

--- server/modules/order/model.order.js
+++ server/modules/order/model.order.js
@@ -3,21 +3,101 @@ const { ObjectId } = mongoose.Schema;
 
 const OrderSchema = new mongoose.Schema(
   {
-    products: [
+    shippingInfo: {
+      phoneNo: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+      area: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+        default: "VietNam",
+      },
+    },
+    orderItems: [
       {
+        saved_name: {
+          type: String,
+          required: true,
+        },
+        saved_quantity: {
+          type: Number,
+          required: true,
+        },
+        saved_image: {
+          type: String,
+          required: true,
+        },
+        saved_price: {
+          type: Number,
+          required: true,
+        },
+        saved_variant: {
+          type: String,
+          required: true,
+          default: "white,#fff",
+        },
         product: {
           type: ObjectId,
+          required: true,
           ref: "Product",
         },
-        count: Number,
-        color: String,
+        variant: {
+          type: ObjectId,
+          required: true,
+          ref: "Variant",
+        },
       },
     ],
-    paymentIntent: {},
+    paymentInfo: {
+      id: {
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+    },
+    paidAt: {
+      type: Date,
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    deliveredAt: {
+      type: Date,
+    },
     orderStatus: {
       type: String,
-      default: "Not Processed",
-      enum: ["Not Processed", "Processing", "Dispatched", "Cancelled", "Completed"],
+      required: true,
+      default: "Processing",
     },
     createdBy: { type: ObjectId, ref: "User" },
   },
--- server/modules/order/order.controller.js
+++ server/modules/order/order.controller.js
@@ -1,15 +1,128 @@
 const Order = require("./model.order");
 const User = require("../user/model.user");
+const Product = require("../product/model.product");
 
 exports.createOrder = async (req, res) => {
+  try {
+    const { orderItems, shippingInfo, itemsPrice, shippingPrice, totalPrice, paymentInfo } =
+      req.body;
 
+    const order = await new Order({
+      orderItems,
+      shippingInfo,
+      itemsPrice,
+      shippingPrice,
+      totalPrice,
+      paymentInfo,
+      paidAt: Date.now(),
+      user: req.user._id,
+    }).save();
+
+    res.status(200).json({
+      success: true,
+      data: order,
+    });
+  } catch (err) {
+    res.status(err?.status || 400).json({ success: false, err: err.message });
+  }
 };
 
-exports.orders = async (req, res) => {
+exports.getSingleOrder = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const foundOrder = await Order.findById(orderId).populate("createdBy", "_id name mail picture");
 
+    if (!foundOrder) throw { status: 404, message: `No Order found with ID:${orderId}` };
+
+    res.status(200).json({
+      success: true,
+      data: foundOrder,
+    });
+  } catch (err) {
+    res.status(err?.status || 400).json({ success: false, err: err.message });
+  }
 };
 
+exports.getMyOrders = async (req, res) => {
+  try {
+    const foundOrders = await Order.find({ createdBy: req.user.id });
 
-exports.createCashOrder = async (req, res) => {
-
+    res.status(200).json({
+      success: true,
+      data: foundOrders,
+    });
+  } catch (err) {
+    res.status(err?.status || 400).json({ success: false, err: err.message });
+  }
 };
+
+exports.getAllOrders = async (req, res) => {
+  try {
+    const foundOrders = await Order.find();
+
+    let totalAmount = 0;
+
+    foundOrders.forEach((order) => {
+      totalAmount += order.totalPrice;
+    });
+
+    res.status(200).json({
+      success: true,
+      totalAmount,
+      data: foundOrders,
+    });
+  } catch (err) {
+    res.status(err?.status || 400).json({ success: false, err: err.message });
+  }
+};
+
+exports.updateOrder = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const foundOrder = await Order.findById(orderId);
+
+    if (foundOrder.orderStatus === "Delivered")
+      throw { status: 400, message: `You have already delivered this order:${orderId}` };
+
+    foundOrder.orderItems.forEach(async (item) => {
+      await updateProductQuantity(item.product, item.quantity);
+    });
+
+    (foundOrder.orderStatus = req.body.status), (foundOrder.deliveredAt = Date.now());
+
+    await foundOrder.save();
+
+    res.status(200).json({
+      success: true,
+    });
+  } catch (err) {
+    res.status(err?.status || 400).json({ success: false, err: err.message });
+  }
+};
+
+async function updateProductQuantity(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.quantity = product.quantity - quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+exports.deleteOrder = async (req, res) => {
+  try {
+    const foundOrder = await Order.findById(req.params.id);
+
+    if (!foundOrder) throw { status: 404, message: `No Order found with ID:${orderId}` };
+
+    await foundOrder.remove();
+
+    res.status(200).json({
+      success: true,
+      data: foundOrder,
+    });
+  } catch (err) {
+    res.status(err?.status || 400).json({ success: false, err: err.message });
+  }
+};
+
+exports.createCashOrder = async (req, res) => {};
